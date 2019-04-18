4c864624ebff39906c97de65471a8e4ce4c3a39e
jihunhong <redgee49@gmail.com>
2019-04-18 13:43:58 +0900

    "/profile" mapping added.

---
+++ src/main/java/com/oz/dunpic/Controller/WebRestController.java
@@ -0,0 +1,23 @@
+package com.oz.dunpic.Controller;
+
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class WebRestController {
+
+    private Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
