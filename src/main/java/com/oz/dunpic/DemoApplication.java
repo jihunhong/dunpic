ad7b9268c9b264a14c6733780927e89c6a0edf61
jihunhong <redgee49@gmail.com>
2019-04-18 20:00:25 +0900

    real-db 경로 수정.

--- src/main/java/com/oz/dunpic/DemoApplication.java
+++ src/main/java/com/oz/dunpic/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
 					+ "classpath:application.yml,"
-					+ "/app/config/dunpic/application-db.yml";
+					+ "/home/ec2-user/app/config/dunpic/application-db.yml";
 
 	public static void main(String[] args) {
 			   new SpringApplicationBuilder(DemoApplication.class)
