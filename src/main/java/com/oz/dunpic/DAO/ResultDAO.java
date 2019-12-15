139413697c3141a1ad7ea05cbbf7067a04bda07f
jihunhong <redgee49@gmail.com>
2019-12-15 12:02:12 +0900

    Fix table name

--- src/main/java/com/oz/dunpic/DAO/ResultDAO.java
+++ src/main/java/com/oz/dunpic/DAO/ResultDAO.java
@@ -13,7 +13,7 @@ public interface ResultDAO extends JpaRepository<Result, Integer>{
 	@Query(value="Select * from card a LEFT OUTER JOIN effect on a.id=effect.card_id where effect.groupname=:groupname and effect.card_id in (SELECT a.id FROM card a Inner JOIN effect b ON a.id = b.card_id where part like %:part% and effect=:effect) order by effect.value desc", nativeQuery=true)
 	List<Result> findBySearch(@Param("part") String part, @Param("effect") String effect, @Param("groupname") String groupname);
 
-	@Query(value="SELECT * FROM card a Inner JOIN effect b ON a.id = b.card_id where part like %:part% and effect=:effect order by b.value desc, b.max desc", nativeQuery=true)
+	@Query(value="SELECT * FROM card a Inner JOIN Effect b ON a.id = b.card_id where part like %:part% and effect=:effect order by b.value desc, b.max desc", nativeQuery=true)
 	List<Result> findBySubquery(@Param("part") String part, @Param("effect") String effect);
 
 	@Query(value="SELECT * FROM card a Inner JOIN effect b ON a.id = b.card_id where a.id=:id", nativeQuery=true)
